f5410704510e5e6ee4b6caef6a78a62727b6b92a
AhmedAbdellaa <ahmed.m.abdellah99@gmail.com>
2022-05-12 12:17:49 +0200

    create account function

--- adminPage.c
+++ adminPage.c
@@ -1,1 +1,17 @@
 #include "adminpage.h"
+
+void account_creation(char *name,int age,char *address,int accountid,int status,
+                      float balance,List *l)
+{
+
+    Account account;
+    account.name = name;
+    account.age = age;
+    account.address = address;
+    account.accountid = accountid;
+    account.status = status;
+    account.balance = balance;
+    insertList (0,account,l);
+
+
+}
--- adminPage.h
+++ adminPage.h
@@ -1,5 +1,6 @@
 #ifndef ADMINPAGE_H_INCLUDED
 #define ADMINPAGE_H_INCLUDED
+void account_creation(char *name,int age,char *address,int accountid,int status,
 
 
 #endif // ADMINPAGE_H_INCLUDED
